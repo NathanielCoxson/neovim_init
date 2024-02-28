26f338c9f5894ac876ab79b59e94beb0812d6a98
Nathan Coxson <nathancoxson2739@gmail.com>
2024-02-28 06:47:35 -0500

    added tab autocompletion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,12 +3,13 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+  }),
 })
 
 lsp.on_attach(function(client, bufnr)
